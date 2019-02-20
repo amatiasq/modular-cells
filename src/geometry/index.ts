1f5b275e04ba123f36dfb6e055db714b0c971f41
A. Matías Quezada <amatiasq@gmail.com>
2019-02-20 02:27:57 +0100

    Create geometry module

---
+++ src/geometry/index.ts
@@ -0,0 +1,4 @@
+import Rectangle, { IRectangle } from './Rectangle';
+import SquaredCircle, { ISquaredCircle } from './SquaredCircle';
+
+export { IRectangle, Rectangle, ISquaredCircle, SquaredCircle };
